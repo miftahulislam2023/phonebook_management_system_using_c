8a4ca8adbb574fa6880b9d263c6c8f717988218b
Miftahul Islam <138649177+miftahulislam2023@users.noreply.github.com>
2025-04-28 12:57:50 +0600

    Added a readme and basic files- main.c and file_handling.h

---
+++ file_handling.h
@@ -0,0 +1,11 @@
+//
+// Created by almif on 4/27/2025.
+//
+
+#ifndef FILE_HANDLING_H
+#define FILE_HANDLING_H
+
+#endif //FILE_HANDLING_H
+
+#include <stdlib.h>
+#include <string.h>
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void) {
+    bool isReady = false;
+    printf("%d\n", isReady);
+    
+    return 0;
+}
